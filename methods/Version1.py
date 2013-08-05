e191151a342cf58895e5da0e949d412cb4d76177
Oleksandr <sasha.sochka@gmail.com>
2013-08-06 00:46:33 +0300

    Rename bot to Version1

--- methods/Version1.py
+++ methods/Version1.py
@@ -9,34 +9,34 @@ if __name__ == "__main__":
     from method import Method
 else:
     from methods.method import Method
-    
+
 precomputed = lambda state: {
         ((5, 5, 5, 5, 5, 5), (5, 5, 5, 5, 5, 5)): 1
 }.get(state, None)
 
-class OleksandrMethod(Method):
-    _name = "Oleksandr"
-    _short_name = "Oleksandr"
-    
+class Version1Method(Method):
+    _name = "Version1"
+    _short_name = "Version1"
+
     def __init__(self, player_num, ai_level=1):
-        super(OleksandrMethod, self).__init__(player_num, ai_level)
+        super(Version1Method, self).__init__(player_num, ai_level)
         self._ai_level = min((ai_level+1)/2, 3)
-    
+
     def _other_player(self):
         return (self._player + 1) % 2
-    
+
     def _terminal_test(self, state, player, depth=0):
 #        print player, state.to_string(), depth, self._ai_level
         if depth>=self._ai_level or state.is_finished(player):
             return True
         return False
-    
+
     def _utility(self, state):
         utility = [0,0]
         for player in (self._player,self._other_player()):
             utility[player] = state.player_kalah(player) + sum(state.player_holes(player))
         return utility[self._player] - utility[self._other_player()]
-    
+
     def _max_value(self, state, depth=1):
         if self._terminal_test(state, self._player):
             return self._utility(state)
@@ -45,7 +45,7 @@ class OleksandrMethod(Method):
         for new_state in neighbors:
             value = max(value, self._min_value(new_state['state'], depth))
         return value
-            
+
     def _min_value(self, state, depth=1):
         if self._terminal_test(state, self._other_player(), depth):
 #            print "   "*depth, state.to_string(), self._utility(state)
@@ -55,7 +55,7 @@ class OleksandrMethod(Method):
         for new_state in neighbors:
             value = min(value, self._max_value(new_state['state'], depth+1))
         return value
-    
+
     def make_move(self, state):
         print "Input state:", state.to_string()
         neighbors = state.get_all_neighbors(self._player)
@@ -79,11 +79,11 @@ class OleksandrMethod(Method):
             print best_value, best_state['state'].to_string()
             return best_state['hole'][0]
         return -1
-        
+
 if __name__ == "__main__":
     from state import KalahState
     state = KalahState(0)
     state._kalahs = [6,4]
     state._holes = [[0, 4, 8, 7, 2, 4], [0, 0, 0, 0, 0, 1]]
-    method = OleksandrMethod(1, 1)
+    method = Version1Method(1, 1)
     print method.make_move(state)
